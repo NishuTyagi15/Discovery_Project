3c29f6825758fe014ef0e8919acf993a1f3d4fc7
Nishu Tyagi <tyaginishu7@gmail.com>
2021-10-01 19:34:17 +0530

    [Nishu Tyagi]Refactored:Our Worlds Webpage to fetch data from array using JS

---
+++ js/header_footer.js
@@ -0,0 +1,85 @@
+window.onload = header_content();
+window.onload = footer_content();
+
+
+function header_content() {
+    const headerHtml = `<div class="item-container">
+    <div class="logo-content1">
+        <img class="logo-img1" src="../assets/header/namelogo.svg" alt="Logo Image">
+    </div>
+    <div class="nav-content">
+        <div class="nav"><a href="#">About DLC</a></div>
+        <div class="nav"><a href="../pages/ourWorld.html">Our Worlds</a></div>
+        <div class=""><a href="#">Experiences</a></div>
+        <div><a href="../pages/gallery.html">Gallery</a></div>
+        <div><a href="#">Press</a></div>
+    </div>
+    </div>`
+    
+    document.querySelector('#header').innerHTML = headerHtml;
+}
+
+function footer_content() {
+    const footerHtml = `<footer class="footer-content">
+    <div class="all-foot">
+        <div class="logo-content2">
+            <img class="logo-img2" src="../assets/header/safari-pinned-tab.svg" alt="Logo Image">
+        </div>
+        <div class="foot-items">
+            <h4 class="foot-main">Get in touch</h4>        
+            <ul class="foot-content">
+                <li class="foot-menu">
+                    <a href="tel:(480) 624-5200" class="foot-link">(480) 624-5200</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="mailto:mail@discoverylandco.com" class="foot-link email">mail@discoverylandco.com</a>
+                </li>
+            </ul>
+        </div>
+        <div class="footer-main">
+            <ul class="foot-content --main">
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">About DLC</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="../pages/ourWorld.html" class="foot-link">Our Worlds</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">Experiences</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="../pages/gallery.html" class="foot-link">Gallery</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">Press</a>
+                </li>                    
+            </ul>
+            <ul class="foot-content --sub">
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">Magazine</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">Careers</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">Foundation</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">Company</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">Contact</a>
+                </li>
+                <li class="foot-menu">
+                    <a href="#" class="foot-link">Privacy</a>
+                </li>                    
+            </ul>
+        </div>
+    </div>
+    <div class="foot-end">
+      <p>© Discovery Land Company. All rights reserved.</p>
+    </div>
+    </footer>`
+
+    document.querySelector('#footer').innerHTML = footerHtml;
+}
